4330aa107620727e3d0b71389cae5e2184d5047b
raghav-dwivedi <raghav.dwivedi241@gmail.com>
2024-09-11 17:34:02 +0530

    fix: remove undefined models

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -1,30 +1,8 @@
-import Batches from "./batches";
-import ClassCancellationRequests from "./classCancellationRequests";
-import ClassScheduler from "./classScheduler";
-import CourseByOrderId from "./courseByOrderId";
-import CoursePrice from "./coursePrice";
-import Courses from "./courses";
-import GlimpseClassInfo from "./glimpeClassInfo";
-import ImpartusSession from "./impartusSession";
-import Orders from "./orders";
-import Parents from "./parents";
-import StudentBatchJoinedInfo from "./studentBatchJoinedInfo";
 import StudentCertificate from "./studentCertificate";
 
 // Import other models here
 
 const models = {
-  Batches,
-  ClassCancellationRequests,
-  ClassScheduler,
-  CourseByOrderId,
-  CoursePrice,
-  Courses,
-  GlimpseClassInfo,
-  ImpartusSession,
-  Orders,
-  Parents,
-  StudentBatchJoinedInfo,
   StudentCertificate,
 };
 
